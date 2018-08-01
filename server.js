de87b76aacc299231d51e17ce9f373376a932947
Chryl F <joanferns00@users.noreply.github.com>
2018-08-01 16:19:55 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ module.exports = {
     const app = express()
     const indexPath = path.join(__dirname, 'index.html')
     const publicPath = express.static(path.join(__dirname, 'public'))
-
+    
+    app.use(express.static('public'));
     app.use('/public', publicPath)
     app.get('/', function (_, res) { res.sendFile(indexPath) })
 
